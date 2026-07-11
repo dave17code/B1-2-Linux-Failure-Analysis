11f86d13da34de4a3ad4379b64ee0a712ad23a30
dave17code <dave17code@gmail.com>
2026-07-11 21:12:49 +0900

    260711CodysseyWorkDone

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,7 +5,11 @@ while true; do
   if [ -z "$PID" ]; then
     echo "[$TIME] PROCESS_NOT_FOUND"
   else
-    ps -p "$PID" -o pid=,etime=,%cpu=,%mem=,rss=,vsz=,cmd= | awk -v t="$TIME" '{print "[" t "]", $0}'
+    TOP_LINE=$(top -b -n 1 | grep -E "^[ ]*${PID}\b" | head -n 1)
+    if [ -n "$TOP_LINE" ]; then
+      # 템플릿 요구 컬럼 순서 매핑: [TIME] PID CPU MEM RES
+      echo "$TOP_LINE" | awk -v t="$TIME" '{print "["t"]", "PID:"$1, "CPU:"$9"%", "MEM:"$10"%", "RES:"$6}'
+    fi
   fi
   sleep 0.5
 done
